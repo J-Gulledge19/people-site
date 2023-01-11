338d6a9d3cb0064c86d108a50f24c969830536bc
Josh Gulledge <jgulledge19@gmail.com>
2023-01-10 19:44:12 -0700

    Frontend changed

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -6,8 +6,13 @@ function Show(props) {
   return (
     <div className="person">
       <h1>{person.name}</h1>
-      <h2>{person.title}</h2>
       <img src={person.image} alt={person.name} />
+      <h2>{person.title}</h2>
+
+      <Form action={`/delete/${person._id}`} method="post">
+      <input type="submit" value={`delete ${person.name}`} />
+      </Form>
+      <br></br>
 
       <h2>Update {person.name}</h2>
       <Form action={`/update/${person._id}`} method="post">
@@ -16,10 +21,7 @@ function Show(props) {
         <input type="input" name="title" placeholder="person's title" defaultValue={person.title} />
         <input type="submit" value={`update ${person.name}`} />
       </Form>
-      <h2>Delete Person</h2>
-      <Form action={`/delete/${person._id}`} method="post">
-      <input type="submit" value={`delete ${person.name}`} />
-      </Form>
+    
     </div>
   );
 }
